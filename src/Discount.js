c752fd2d53c6885c849e83bd4ced0f0cbdd6a654
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-11-11 00:38:37 +0900

    ✨ feat($Discount): 날짜 계산 로직 추가

--- src/Discount.js
+++ src/Discount.js
@@ -1,27 +1,48 @@
-import OutputView from './View/OutputView'
+import OutputView from './View/OutputView.js'
 
 class Discount {
-  #date
-  #orders
-  #discounts
-  #total
+  #date;
+  #orders;
+  #discounts;
+  #total;
   
   constructor(date, orders, total) {
-    this.#date = Number(date)
-    this.#orders = orders
-    this.#total = Number(total)
-    this.isValid = Number(total) > 10000
+    this.#date = Number(date);
+    this.#orders = orders;
+    this.#total = Number(total);
+    this.isValid = Number(total) > 10000;
     this.#discounts = {
       forChristmas: 0,
       forDaily: 0,
       forWeekend: 0,
       forStarDays: 0,
+    };
+  }
+
+  getWeekendByDay(dayOfWeek) {
+    const days = [];
+
+    for (let day = 1; day <= 31; day++) {
+      const today = new Date(2023, 11, day);
+      if (today.getDay() === dayOfWeek) {
+        days.push(day);
+      }
     }
+
+    return days;
+  }
+  
+  getSunday() {
+    return getWeekendByDay(0);
+  }
+
+  getSaturday() {
+    return getWeekendByDay(6);
   }
 
   calculateChristmasDiscount() {
     if (this.#date >= 1 && this.#date <= 25) {
-      this.#discounts.forChristmas = (this.#date + 9) * 100
+      this.#discounts.forChristmas = (this.#date + 9) * 100;
     }
   }
 }
